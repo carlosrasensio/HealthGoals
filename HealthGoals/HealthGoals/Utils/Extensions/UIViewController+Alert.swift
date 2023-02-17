5920411b48c08e09c86133e7a06523b076545c72
carlosrasensio <carlosrasensio@gmail.com>
2023-02-17 02:54:29 +0100

    Create UIViewController extension to present an alert

---
+++ HealthGoals/HealthGoals/Utils/Extensions/UIViewController+Alert.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+Alert.swift
+//  HealthGoals
+//
+//  Created by Carlos Rodriguez Asensio on 17/2/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+  func showAlert(title: String, message : String) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alert.view.backgroundColor = .red
+    alert.view.alpha = 0.5
+    alert.view.layer.cornerRadius = 15
+    self.present(alert, animated: true)
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+      alert.dismiss(animated: true)
+    }
+  }
+}
