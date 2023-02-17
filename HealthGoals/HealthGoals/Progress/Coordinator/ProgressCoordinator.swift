8f2889f74625d8f2de561d0c0725b5a1dda03284
carlosrasensio <carlosrasensio@gmail.com>
2023-02-17 04:29:27 +0100

    Configure coordinator of the new screen

--- HealthGoals/HealthGoals/Progress/Coordinator/ProgressCoordinator.swift
+++ HealthGoals/HealthGoals/Progress/Coordinator/ProgressCoordinator.swift
@@ -6,17 +6,33 @@
 //
 
 import Foundation
+import UIKit
 
 protocol ProgressCoordinatorProtocol {
-  
+  func createProgressViewController() -> UIViewController
 }
 
 final class ProgressCoordinator {
+  // MARK: Variables
+  private var sourceView: UIViewController?
+  var viewController: UIViewController {
+    createProgressViewController()
+  }
+  private var goal: Goal?
   
+  // MARK: Initializer
+  init(goal: Goal? = nil) {
+    self.goal = goal
+  }
 }
 
 // MARK: - ProgressCoordinatorProtocol
 
 extension ProgressCoordinator: ProgressCoordinatorProtocol {
-  
+  func createProgressViewController() -> UIViewController {
+    let presenter = ProgressPresenter(goal: goal)
+    let view = ProgressViewController(presenter: presenter, coordinator: ProgressCoordinator())
+    
+    return view
+  }
 }
