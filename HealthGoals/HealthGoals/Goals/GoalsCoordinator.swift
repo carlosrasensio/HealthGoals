09a7fe88a656eb6a98aeda3aa163eee2618b55fd
carlosrasensio <carlosrasensio@gmail.com>
2023-02-17 01:43:31 +0100

    Create coordinator to instantiate the goals view controller

---
+++ HealthGoals/HealthGoals/Goals/GoalsCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  GoalsCoordinator.swift
+//  HealthGoals
+//
+//  Created by Carlos Rodriguez Asensio on 17/2/23.
+//
+
+import Foundation
+import UIKit
+
+protocol GoalsCoordinatorProtocol: AnyObject {
+  func createGoalsViewController() -> UIViewController
+  func setSourceView(_ view: UIViewController?)
+}
+
+final class GoalsCoordinator {
+  // MARK: Variables
+  private var sourceView: UIViewController?
+  var viewController: UIViewController { createGoalsViewController() }
+}
+
+// MARK: - CharactersCollectionCoordinatorProtocol
+
+extension GoalsCoordinator: GoalsCoordinatorProtocol {
+  func createGoalsViewController() -> UIViewController {
+    let view = GoalsViewController(router: GoalsCoordinator())
+
+    return view
+  }
+  
+  func setSourceView(_ view: UIViewController?) {
+    guard let view else { return }
+    sourceView = view
+  }
+}
